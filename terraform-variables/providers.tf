9d52741a26c524eedf0a91db368833e7e10ff965
user-raghava <raghavendragovindu007@gmail.com>
2025-11-18 21:47:34 +0530

    created azure terraform

--- terraform-variables/providers.tf
+++ terraform-variables/providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.11.0"
+  required_version = ">= 1.11.0"  # specify terraform version
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0.0"
+      version = ">= 6.0.0"    # >= greater than equal to 6.0.0
     }
   }
 }
