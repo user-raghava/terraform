b5de34192d0fae6b4d4f438e29e220dfbcdbf258
user-raghava <raghavendragovindu007@gmail.com>
2025-11-24 00:18:33 +0530

    output

--- datasource/main.tf
+++ datasource/main.tf
@@ -1,3 +1,4 @@
+# Create a VPC
 resource "aws_vpc" "test_vpc" {
   cidr_block = "10.10.0.0/16"
 
@@ -6,3 +7,7 @@ resource "aws_vpc" "test_vpc" {
 # Retrieve information about the VPC created above
 data "aws_vpc" "test" {}
 
+# Output the VPC ID
+output "vpc_id" {
+  value = data.aws_vpc.test.id
+}
